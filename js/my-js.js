c0d3b73b4e4c0735db9242a6350a23002e1747af
peonerrrr <peonerrrr@gmail.com>
2018-12-21 14:43:39 +0300

    animation button menu toggle

--- js/my-js.js
+++ js/my-js.js
@@ -1,10 +1,35 @@
 $('.show-btn').click(function() {
-	  $( ".navbar-items" ).slideToggle("slow");
-	  $( ".button-close" ).slideToggle("slow");
-	  $( ".show-btn" ).slideToggle(1200);
-	});
-$('.button-close').click(function() {
-	  $( ".navbar-items" ).slideToggle("slow");
-	  $( ".button-close" ).slideToggle("slow");
-	  $( ".show-btn" ).slideToggle(1200);
-	});
+	slider();
+	$( ".navbar-items" ).slideToggle("slow");	
+		  // $( ".button-close" ).slideToggle("slow");
+	  // $( ".show-btn" ).slideToggle(1200);
+});
+function slider() {
+	if (document.querySelector('.navbar-items').style.display == 'block'){
+		$('.top-line').removeClass('rotateShowY');
+		$('.center-line').removeClass('displayShow');				
+		$('.bottom-line').removeClass('rotateShowX');
+
+		$('.top-line').addClass('rotateHideY');
+		$('.center-line').addClass('displayHide');				
+		$('.bottom-line').addClass('rotateHideX');
+	}	
+	else {
+		$('.top-line').addClass('rotateShowY');
+		$('.center-line').addClass('displayShow');				
+		$('.bottom-line').addClass('rotateShowX');
+
+		$('.top-line').removeClass('rotateHideY');
+		$('.center-line').removeClass('displayHide');				
+		$('.bottom-line').removeClass('rotateHideX');
+		// $('.center-line').addClass('displayHide');		
+		// $('.top-line').addClass('rotateHideY');		
+		// $('.bottom-line').addClass('rotateHideX');
+	}
+}
+// $('.button-close').click(function() {
+// 	  $( ".navbar-items" ).slideToggle("slow");
+// 	  $( ".button-close" ).slideToggle("slow");
+// 	  $( ".show-btn" ).slideToggle(1200);
+// 	});
+
