e6ff5e9af70a7efff7d86e32182adeed5c43063a
peonerrrr <peonerrrr@gmail.com>
2018-12-21 10:50:45 +0300

    animation button menu

--- js/my-js.js
+++ js/my-js.js
@@ -1,3 +1,10 @@
 $('.show-btn').click(function() {
-	  $( ".navbar-items" ).slideToggle();
+	  $( ".navbar-items" ).slideToggle("slow");
+	  $( ".button-close" ).slideToggle("slow");
+	  $( ".show-btn" ).slideToggle(1200);
+	});
+$('.button-close').click(function() {
+	  $( ".navbar-items" ).slideToggle("slow");
+	  $( ".button-close" ).slideToggle("slow");
+	  $( ".show-btn" ).slideToggle(1200);
 	});
